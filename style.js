bf1278d7fd5b176490547840c9f2343039775bea
harishkumar <harishkumar2004harish@gmail.com>
2024-09-12 16:33:12 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,17 @@
+var sidebar=document.querySelector(".side-bars")
+function opennavbar(){
+    sidebar.style.left="0%"
+}
+function closebar(){
+    sidebar.style.left="-50%"
+}
+
+var sidenavbar=document.querySelector(".sidenav-details")
+function openbar(){
+    sidenavbar.style.left="0%"
+}
+function closenavbar(){
+    sidenavbar.style.left="-50%"
+}
+
+
